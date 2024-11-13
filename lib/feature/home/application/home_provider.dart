c4b4d687d6cde96c41567ba15dc7e2d9edb6291b
Yiğit TİLKİ <yigittilkiw@gmail.com>
2024-11-13 17:52:52 +0300

    config

--- lib/feature/home/application/home_provider.dart
+++ lib/feature/home/application/home_provider.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mdp2/feature/home/application/home_notifier.dart';
 import 'package:mdp2/feature/home/data/home_repository.dart';
 import 'package:mdp2/feature/home/domain/home_state.dart';
+import 'package:mdp2/feature/home/domain/interface/i_home_repository.dart';
+import 'package:mdp2/product/base/base_notifier.dart';
 import 'package:mdp2/product/providers/dio_provider.dart';
 import 'package:mdp2/product/providers/logger_provider.dart';
 
@@ -10,3 +11,17 @@ final homeProvider = StateNotifierProvider<HomeNotifier, HomeState>((ref) {
   final logger = ref.watch(loggerProvider);
   return HomeNotifier(homeRepository: HomeRepository(dio: dio, logger: logger));
 });
+
+class HomeNotifier extends BaseNotifier<HomeState> {
+  HomeNotifier({
+    required IHomeRepository homeRepository,
+  })  : _homeRepository = homeRepository,
+        super(const HomeState());
+
+  late final IHomeRepository _homeRepository;
+
+  Future<void> getUsers() async {
+    final data = await _homeRepository.getUsers();
+    emit(state.copyWith(users: data));
+  }
+}
