b725d28de64b685d6786a0b4f9b164f49d2767ee
Yiğit TİLKİ <yigittilkiw@gmail.com>
2024-11-14 18:05:10 +0300

    k

--- lib/feature/profile/viewmodel/profile_view_model.dart
+++ lib/feature/profile/viewmodel/profile_view_model.dart
@@ -1,6 +1,19 @@
+import 'package:mdp2/feature/posts/domain/interface/i_posts_repository.dart';
+import 'package:mdp2/feature/profile/domain/interface/i_profile_repository.dart';
+import 'package:mdp2/feature/profile/model/album_model.dart';
+import 'package:mdp2/feature/profile/model/post_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'profile_view_model.g.dart';
+class ProfileViewModel implements IProfileRepository {
+  @override
+  Future<AsyncValue<List<AlbumModel>>> getUserAlbumsWithPhotos(String userId) {
+    // TODO: implement getUserAlbumsWithPhotos
+    throw UnimplementedError();
+  }
 
-@riverpod
-class ProfileViewModel extends _$ProfileViewModel {}
+  @override
+  Future<AsyncValue<List<PostModel>>> getUserPosts(String userId) {
+    // TODO: implement getUserPosts
+    throw UnimplementedError();
+  }
+}
