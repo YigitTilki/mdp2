08469c26e0402ebd5ea6c5dcfbad161e7493f873
Yiğit TİLKİ <yigittilkiw@gmail.com>
2024-11-19 10:17:11 +0300

    app icon added

--- lib/feature/splash/view/splash_view.dart
+++ lib/feature/splash/view/splash_view.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:mdp2/feature/splash/view/widgets/no_net_dialog.dart';
 import 'package:mdp2/feature/splash/view_model/splash_view_model.dart';
 import 'package:mdp2/main.dart';
+import 'package:mdp2/product/helper/app_spacer.dart';
 import 'package:mdp2/product/navigation/app_router.dart';
 
 @RoutePage()
@@ -22,7 +24,14 @@ class _SplashViewState extends ConsumerState<SplashView> {
     return Scaffold(
       body: Center(
         child: viewModel.maybeWhen(
-          loading: CircularProgressIndicator.new,
+          loading: () => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset('assets/icons/app_icon.svg'),
+              const AppSpacer.vertical20(),
+              const CircularProgressIndicator(),
+            ],
+          ),
           data: (data) {
             if (data == true) {
               appRouter.replace(const HomeRoute());
